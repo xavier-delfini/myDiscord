10e98a0a42676e5bfaa06504c010c7c0952235a8
Yassine-Fellous <91788542+Yassine-Fellous@users.noreply.github.com>
2023-03-22 23:16:44 +0100

    en cours

---
+++ class/Database.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+
+class Database:
+    def __init__(self):
+        self.__user = 'root'
+        self.__password = 'root'
+        self.__host = '127.0.0.1'
+        self.__db = 'MyDiscord'
+        self.__db = mysql.connector.connect(user=self.__user, password=self.__password, host=self.__host, database=self.__db)
+
+    def db(self):
+        return self.__db
