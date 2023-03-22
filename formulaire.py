723d27310e0f0d10e236e785af2ce1c75f594ac2
nicolas-fanjas-mercere <91787661+nicolas-fanjas-mercere@users.noreply.github.com>
2023-03-22 16:18:54 +0100

    terminé
    termined

--- formulaire.py
+++ formulaire.py
@@ -11,46 +11,47 @@ class Formulaire:
         self.root.title("Formulaire")
         self.root.geometry("1920x1080+0+0")
 
-        frame1 = Frame(self.root, bg="grey")
+        frame1 = Frame(self.root, bg="lightgrey")
         frame1.place(x=500, y=200, width=700, height=500)
 
-        title = Label(frame1, text="Créer un compte", font=("arial",20, "bold"), bg="grey", fg="orange")
+        title = Label(frame1, text="Créer un compte", font=("arial",20, "bold"), bg="lightgrey", fg="black")
         title.place(x=50, y=30)
 
-        aff_prenom = Label(frame1, text="prénom", font=("arial",20, "bold"), bg="grey", fg="black")
+        aff_prenom = Label(frame1, text="prénom", font=("arial",20, "bold"), bg="lightgrey", fg="black")
         aff_prenom.place(x=50, y=100)
         self.ecri_prenom = Entry(frame1, font=("arial",), bg="lightgrey")
         self.ecri_prenom.place(x=50, y=130)
 
-        aff_nom = Label(frame1, text="nom", font=("arial", 20, "bold"), bg="grey", fg="black")
+        aff_nom = Label(frame1, text="nom", font=("arial", 20, "bold"), bg="lightgrey", fg="black")
         aff_nom.place(x=370, y=100)
         self.ecri_nom = Entry(frame1, font=("arial",), bg="lightgrey")
         self.ecri_nom.place(x=370, y=130)
 
-        aff_email = Label(frame1, text="email", font=("arial", 20, "bold"), bg="grey", fg="black")
+        aff_email = Label(frame1, text="email", font=("arial", 20, "bold"), bg="lightgrey", fg="black")
         aff_email.place(x=50, y=160)
         self.ecri_email = Entry(frame1, font=("arial",), bg="lightgrey")
         self.ecri_email.place(x=50, y=190)
 
-        aff_mdp = Label(frame1, text="mdp", font=("arial", 20, "bold"), bg="grey", fg="black")
+        aff_mdp = Label(frame1, text="mdp", font=("arial", 20, "bold"), bg="lightgrey", fg="black")
         aff_mdp.place(x=370, y=160)
         self.ecri_mdp = Entry(frame1,show="*", font=("arial",), bg="lightgrey")
         self.ecri_mdp.place(x=370, y=190)
 
-        aff_confmdp = Label(frame1, text="Confirme le mdp", font=("arial", 20, "bold"), bg="grey", fg="black")
+        aff_confmdp = Label(frame1, text="Confirme le mdp", font=("arial", 20, "bold"), bg="lightgrey", fg="black")
         aff_confmdp.place(x=50, y=240)
         self.ecri_confmdp = Entry(frame1,show="*", font=("arial",), bg="lightgrey")
         self.ecri_confmdp.place(x=50, y=270)
+
         btn = Button(frame1, text="créer", cursor="hand2",command=self.creer, font=("arial", 15,), bg="cyan", fg="black")
         btn.place(x=250, y=430)
-        btn2 = Button(frame1, text="connexion", cursor="hand2", font=("arial", 15,), bg="cyan", fg="black")
+        btn2 = Button(frame1, text="connexion", cursor="hand2",command=self.fenetre_creer, font=("arial", 15,), bg="cyan", fg="black")
         btn2.place(x=550, y=250)
 
     def creer(self):
         if self.ecri_prenom.get()=="" or self.ecri_email.get()=="" or self.ecri_nom.get()=="" or self.ecri_mdp.get()=="" or self.ecri_confmdp.get()=="":
             messagebox.showerror("Erreur","à quoi tu joues ? tu veux que jte ban ip fdp ? Allez hop hop faut tout remplir sans se tromper", parent=self.root)
-        elif  self.ecri_prenom.get()==" " or self.ecri_email.get()=="" or self.ecri_nom.get()=="" or self.ecri_mdp.get()=="" or self.ecri_confmdp.get()=="":
-            messagebox.showerror("Erreur","à quoi tu joues ? tu veux que jte ban ip fdp ? Allez hop hop faut tout remplir sans se tromper", parent=self.root)
+        elif  " " in self.ecri_prenom.get()or  " " in self.ecri_nom.get():
+            messagebox.showerror("Erreur","Pas d'espace ou autre caracter pour les nom ou prénom juste -", parent=self.root)
 
         elif self.ecri_mdp.get() !=self.ecri_confmdp.get():
             messagebox.showerror("Erreur","Euuuuh je crois tu t'es trompé mec surement 123 et 132 ça arrive de confondre les deux quand ton qi est negatif", parent=self.root)
@@ -72,6 +73,12 @@ class Formulaire:
 
             except Exception as es:
                 messagebox.showerror("Erreur", f"Erreur de connexion {str(es)}", parent=self.root)
+
+
+    def fenetre_creer(self):
+        self.root.destroy()
+        import login
+
 
 
 
