6c1b8eb927a12031fb7b4397e2db12285ef844df
xavier-delfini <xavier.delfini@laplateforme.io>
2023-03-29 10:55:07 +0200

    Simplification du code de lancement du serveur

--- Server/ServerStart.py
+++ Server/ServerStart.py
@@ -10,23 +10,24 @@ def instance_create(c):
     instance_object.Main()
     del instance_object
     concurrent.futures.Future.done()
-def main():
-    host = ""
-    port = 15555
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((host, port))
-    print("Le serveur à démarrer sur le port", port, "et attend des clients")
-    server.listen(5)
-    print("Le serveur attend une nouvelle connexion")
 
-    #Création d'une instance de la fonction instance_create, il permet la connexion de plusieurs hotes en simultanée
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        while True:
-            c, addr = server.accept()#Attente d'une connexion
-            print('Connexion de :', addr[0], ':', addr[1])
-            executor.submit(instance_create, c)#Création d'une instance utilisant la fonction instance_create
-            #Et qui passe en argument l'objet de connexion
-            #Cette action créer un thread pour l'utilisateur qui vient de démarrer une connexion avec notre server
-    server.close()
-main()
+host = ""
+port = 15555
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+print("Le serveur à démarrer sur le port", port, "et attend des clients")
+server.listen(5)
+print("Le serveur attend une nouvelle connexion")
+
+#Création d'une instance de la fonction instance_create, il permet la connexion de plusieurs hotes en simultanée
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    while True:
+        c, addr = server.accept()#Attente d'une connexion
+        print('Connexion de :', addr[0], ':', addr[1])
+        executor.submit(instance_create, c)#Création d'une instance utilisant la fonction instance_create
+        #Et qui passe en argument l'objet de connexion
+        #Cette action créer un thread pour l'utilisateur qui vient de démarrer une connexion avec notre server
+server.close()
+
